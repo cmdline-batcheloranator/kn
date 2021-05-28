2436ff8bc51691afc243d9ae4c8aa354d7f1692e
cmdline-batcheloranator <84193135+cmdline-batcheloranator@users.noreply.github.com>
2021-05-28 09:14:55 +0100

    Create printCPmodel95.py

---
+++ recursion/Print/printCPmodel95.py
@@ -0,0 +1,12 @@
+my_list = [1, 2, 3, 4, 5]
+
+
+def recursion_print_list(input_list, target_index = None):
+    list_end = len(input_list) - 1
+    target_index = 0 if target_index is None else target_index
+
+    print(input_list[target_index])
+    if target_index == list_end:
+        return
+    else:
+        recursion_print_list(input_list, target_index + 1)
