579d55feb5a16edf3f05d6a1972a6dc6518044f6
cmdline-batcheloranator <84193135+cmdline-batcheloranator@users.noreply.github.com>
2021-05-28 09:04:46 +0100

    Update printNegative.py

--- recursion/printNegative.py
+++ recursion/printNegative.py
@@ -1,10 +1,10 @@
 lst = [1, 2, 3]
 
-def iterate(lst, l = len(lst)):
+def iterPrint(lst, l = len(lst)):
     i = l - 1
     if i < 0:
         return 0
     print(lst[i])
-    return iterate(lst, i)
+    return iterPrint(lst, i)
 
-iterate(lst)
+iterPrint(lst)
