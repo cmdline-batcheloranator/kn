8ee849dd4dcb19e12f92ae826c25c4a20c5baf6e
apple <apple@apples-MacBook-Air-3.local>
2021-05-29 19:10:43 +0100

    x

--- recursion/Print/printCPmodel95.py
+++ recursion/Print/printCPmodel95.py
@@ -1,12 +1,13 @@
-my_list = [1, 2, 3, 4, 5]
+input_list = [1, 2, 3, 4, 5]
 
 
 def recursion_print_list(input_list, target_index = None):
     list_end = len(input_list) - 1
     target_index = 0 if target_index is None else target_index
-
     print(input_list[target_index])
     if target_index == list_end:
         return
     else:
         recursion_print_list(input_list, target_index + 1)
+
+recursion_print_list(input_list)
--- recursion/Print/printPositive.py
+++ recursion/Print/printPositive.py
@@ -1,8 +1,8 @@
-lst = [1, 2, 3]
+lst = [1, 2, 3,  4,  5]
 
 def iterPrint(lst, i = 0):
     l = len(lst) 
-    if i is l:
+    if i  == l:
         return 0
     print(lst[i])
     iterPrint(lst, i + 1)
